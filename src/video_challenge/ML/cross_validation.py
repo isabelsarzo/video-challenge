e40bb5055f549d8fcb3ab1d1eb5e42816e347530
Isabel <isabel.sarzo@gmail.com>
2026-02-22 00:20:08 -0500

    added tabnet and changed ncv to cv

---
+++ src/video_challenge/ML/cross_validation.py
@@ -0,0 +1,275 @@
+import pandas as pd
+import numpy as np
+import pickle
+import logging
+from datetime import datetime
+import wandb
+import optuna
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import StratifiedGroupKFold
+from sklearn.metrics import precision_score, recall_score, f1_score
+from sklearn.feature_selection import SelectKBest, mutual_info_classif
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from pytorch_tabnet.tab_model import TabNetClassifier
+import torch
+import xgboost as xgb
+from .optuna_objective import objective, reset_wandb_env
+
+from ..feature_extraction.pull_features import pull_features
+from . import config as cfg
+
+
+t1 = datetime.now()
+t1_timestamp = datetime.strftime(t1, "%Y%b%d-%H%M%S")
+
+print(
+    f"Initiated Cross-Validation script on {datetime.strftime(t1, '%d-%b-%Y %H:%M:%S')}"
+)
+
+wandb.login()
+
+# results dir config
+RESULTS = cfg.paths["results_root"] / f"results_run_{t1_timestamp}"
+RESULTS.mkdir(parents=True, exist_ok=False)
+SPLITS = RESULTS / "splits"
+SPLITS.mkdir(parents=True, exist_ok=True)
+OPTUNA_DIR = RESULTS / "optuna"
+OPTUNA_DIR.mkdir(parents=True, exist_ok=True)
+optuna_db_file = OPTUNA_DIR / "optuna.db"
+optuna_storage = f"sqlite:///{optuna_db_file}"
+print(f"Results will be saved in: {RESULTS}")
+
+# log config
+log_dir = RESULTS / f"log_{t1_timestamp}.log"
+logging.basicConfig(filename=log_dir, level=logging.INFO, format="%(message)s")
+logger = logging.getLogger(__name__)
+logger.info(f"CV started on: {datetime.strftime(t1, '%d-%b-%Y %H:%M:%S')}")
+
+# load dataset
+print("Loading dataset...")
+features, desc = pull_features(
+    dir=cfg.paths["features_dir"], labels=cfg.paths["labels_file"]
+)
+
+print(f"Dataset shape: {features.shape}")
+print("Success!")
+logger.info(f"Dataset: {desc}")
+
+# cv configs
+cv = StratifiedGroupKFold(n_splits=cfg.cv_folds)
+
+# split data into training and testing (held-out test set)
+split = StratifiedGroupKFold(n_splits=5, shuffle=True, random_state=18) # just keep first fold
+train_idx, test_idx = next(
+    split.split(
+        features, 
+        features["label"], 
+        groups=features["child_id"])
+)
+train_data = features.iloc[train_idx]
+test_data = features.iloc[test_idx]
+
+# -------- track splits --------
+patients_in_training_set = train_data["child_id"].unique().tolist()
+patients_in_testing_set = test_data["child_id"].unique().tolist()
+logger.info(f"Patients in training set: {patients_in_training_set}")
+logger.info(f"-------- n = {train_data["child_id"].nunique()}")
+logger.info(f"Patients in testing set: {patients_in_testing_set}")
+logger.info(f"-------- n = {test_data["child_id"].nunique()}")
+
+pd.DataFrame(patients_in_training_set, columns=["child_id"]).to_csv(
+    SPLITS / "patients_training.csv", index=False
+)
+pd.DataFrame(patients_in_testing_set, columns=["child_id"]).to_csv(
+    SPLITS / "patients_testing.csv", index=False
+)
+
+# cv splits
+for i, (subtrain, val) in enumerate(
+    cv.split(
+        X=train_data, y=train_data["label"], groups=train_data["child_id"]
+    ),
+    start=1,
+):
+    subtrain_patients = list(set(train_data["child_id"].values[subtrain]))
+    val_patients = list(set(train_data["child_id"].values[val]))
+    logger.info(f"----- CV FOLD {i}/{cv.get_n_splits()}:")
+    logger.info(f"------ Patients in sub-training set: {subtrain_patients}")
+    logger.info(f"--------- n = {len(subtrain_patients)}")
+    logger.info(f"------ Patients in validation set: {val_patients}")
+    logger.info(f"--------- n = {len(val_patients)}")
+
+    pd.DataFrame(subtrain_patients, columns=["child_id"]).to_csv(
+        SPLITS / f"cv_fold{i}_training.csv", index=False
+    )
+    pd.DataFrame(val_patients, columns=["child_id"]).to_csv(
+        SPLITS / f"cv_fold{i}_validation.csv", index=False
+    )
+
+    # get subtrain labels
+    y_subtrain = train_data["label"].iloc[subtrain]
+
+    # count positives and negatives
+    n_pos = (y_subtrain == 1).sum()
+    n_neg = (y_subtrain == 0).sum()
+
+    print(f"------ Positive samples in sub-training set: {n_pos}")
+    print(f"------ Negative samples in sub-training set: {n_neg}")
+
+    y_val = train_data["label"].iloc[val]
+    n_pos_val = (y_val == 1).sum()
+    n_neg_val = (y_val == 0).sum()
+
+    print(f"------ Positive samples in validation set: {n_pos_val}")
+    print(f"------ Negative samples in validation set: {n_neg_val}")
+
+# ------ prepare data for pipeline ------
+x_train = train_data.drop(columns=["segment_name", "segment_id", "child_id", "label"])
+y_train = train_data["label"].to_numpy()
+
+x_test = test_data.drop(columns=["segment_name", "segment_id", "child_id", "label"])
+y_true = test_data["label"].to_numpy()
+
+# execute optuna optimization for cv
+logger.info("Executing Optuna optimization for CV...")
+
+study = optuna.create_study(
+    study_name="CV",
+    storage=optuna_storage,
+    load_if_exists=True,
+    direction="maximize",
+    sampler=optuna.samplers.TPESampler(seed=18),
+    pruner=optuna.pruners.MedianPruner(
+        n_startup_trials=5,
+        n_warmup_steps=50,
+    ),
+)
+
+study.optimize(
+    lambda trial: objective(
+        trial,
+        x_train,
+        y_train,
+        train_data["child_id"].values,
+        cv,
+        wandb_dir=RESULTS,
+        model_type=cfg.MODEL_TYPE
+    ),
+    n_trials=100, # number of hp configs
+)
+
+# Get best hyperparms
+best_params = study.best_params
+best_k = best_params.pop("k")
+
+reset_wandb_env()
+run_name = "CV"
+group_name_wandb = "cv_model"
+wandb.init(
+    project=cfg.project_name,
+    name=run_name,
+    config=best_params,
+    reinit=True,
+    group=group_name_wandb,
+    dir=RESULTS,
+)
+
+# Get best model with best hyperparams
+preprocessor = Pipeline(
+    [
+        ("imputer", SimpleImputer(strategy="mean")),
+        ("scaler", MinMaxScaler()),
+        ("feature_selection", SelectKBest(score_func=mutual_info_classif, k=best_k)),
+    ]
+)
+
+X_train_transformed = preprocessor.fit_transform(x_train, y_train)
+X_test_transformed = preprocessor.transform(x_test)
+
+if cfg.MODEL_TYPE == "xgboost":
+    best_model = xgb.XGBClassifier(
+        **best_params,
+        n_estimators=study.best_trial.user_attrs.get(
+            "best_iteration", 300
+        ),  # safe fallback
+        objective="binary:logistic",
+        eval_metric="logloss",
+        tree_method="hist",
+        n_jobs=32,
+        verbosity=1,
+        random_state=18,
+        early_stopping_rounds=50,
+        callbacks=[
+            wandb.xgboost.WandbCallback(),
+        ],
+    )
+
+    best_model.fit(
+        X_train_transformed,
+        y_train,
+        eval_set=[(X_test_transformed, y_true)],
+    )
+
+elif cfg.MODEL_TYPE == "tabnet":
+    if "lr" in best_params:
+        best_params["optimizer_params"] = dict(lr=best_params.pop("lr"))
+    if "n_da" in best_params:
+        n_da = best_params.pop("n_da")
+        best_params["n_d"] = n_da
+        best_params["n_a"] = n_da
+    
+    best_model = TabNetClassifier(
+        **best_params,
+        seed=18,
+        device_name='cuda' if torch.cuda.is_available() else 'cpu'
+    )
+
+    best_model.fit(
+        X_train=X_train_transformed, y_train=y_train,
+        eval_set=[(X_test_transformed, y_true)],
+        eval_name=['test'], eval_metric=['logloss'],
+        max_epochs=200, patience=30,
+        batch_size=256, virtual_batch_size=128
+    )
+
+pipeline = Pipeline(steps=preprocessor.steps + [("model", best_model)])
+
+# get best model and save it
+model_path = RESULTS / f"CV_pipeline_{cfg.MODEL_TYPE}.pkl"
+with open(model_path, "wb") as f:
+    pickle.dump(pipeline, f)
+
+# get best model predictions on testing set
+y_pred = best_model.predict(X_test_transformed)
+
+# -------- compute scores --------
+recall = recall_score(y_true, y_pred, zero_division=np.nan)
+precision = precision_score(y_true, y_pred, zero_division=np.nan)
+f1score = f1_score(y_true, y_pred, zero_division=0)   
+
+# save best hyperparams
+best_params = study.best_params
+best_params = pd.DataFrame([best_params])
+best_params.to_csv(RESULTS / "best_params.csv", index=False)
+
+scores = {
+    "Recall": recall,
+    "Precision": precision,
+    "F1-score": f1score,
+}
+
+wandb.log(scores)
+wandb.finish()
+
+# save scores
+scores = pd.DataFrame([scores])
+logger.info(f"Scores on held-out test set: {scores}")
+scores.to_csv(RESULTS / "scores.csv")
+
+t2 = datetime.now()
+t2_timestamp = datetime.strftime(t2, "%d-%b-%Y %H:%M:%S")
+logger.info("----------------------------------------")
+logger.info(f"Finished on: {t2_timestamp}")
+logger.info(f"Training duration: {t2 - t1}")
+logging.shutdown()
--- src/video_challenge/ML/optuna_objective.py
+++ src/video_challenge/ML/optuna_objective.py
@@ -8,7 +8,8 @@ from sklearn.metrics import f1_score
 from sklearn.feature_selection import SelectKBest, mutual_info_classif
 from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
-
+from pytorch_tabnet.tab_model import TabNetClassifier
+import torch
 import xgboost as xgb
 from optuna.integration import XGBoostPruningCallback
 
@@ -23,27 +24,42 @@ def reset_wandb_env():
             del os.environ[key]
 
 
-def objective(trial, X, y, groups, cv, outer_fold, wandb_dir):
-    params = {
-        "max_depth": trial.suggest_int("max_depth", 3, 10),
-        "min_child_weight": trial.suggest_float(
-            "min_child_weight", 1.0, 10.0, log=True
-        ),
-        "learning_rate": trial.suggest_float("learning_rate", 1e-3, 0.3, log=True),
-        "gamma": trial.suggest_float("gamma", 1e-8, 5.0, log=True),
-        "reg_alpha": trial.suggest_float("reg_alpha", 1e-8, 1.0, log=True),
-        "reg_lambda": trial.suggest_float("reg_lambda", 1e-3, 10.0, log=True),
-        "max_delta_step": trial.suggest_int("max_delta_step", 0, 10),
-        "scale_pos_weight": trial.suggest_float(
-            "scale_pos_weight", 1.0, 10.0, log=True
-        ),
-    }
+def objective(trial, X, y, groups, cv, wandb_dir, model_type="xgboost"):
+    if model_type == "xgboost":
+        params = {
+            "max_depth": trial.suggest_int("max_depth", 3, 10),
+            "min_child_weight": trial.suggest_float(
+                "min_child_weight", 1.0, 10.0, log=True
+            ),
+            "learning_rate": trial.suggest_float("learning_rate", 1e-3, 0.3, log=True),
+            "gamma": trial.suggest_float("gamma", 1e-8, 5.0, log=True),
+            "reg_alpha": trial.suggest_float("reg_alpha", 1e-8, 1.0, log=True),
+            "reg_lambda": trial.suggest_float("reg_lambda", 1e-3, 10.0, log=True),
+            "max_delta_step": trial.suggest_int("max_delta_step", 0, 10),
+            "scale_pos_weight": trial.suggest_float(
+                "scale_pos_weight", 1.0, 10.0, log=True
+            ),
+        }
+    elif model_type == "tabnet":
+        n_da = trial.suggest_int("n_da", 8, 64, step=8)
+        params = {
+            "n_d": n_da,
+            "n_a": n_da,
+            "n_steps": trial.suggest_int("n_steps", 3, 10),
+            "gamma": trial.suggest_float("gamma", 1.0, 2.0),
+            "n_independent": trial.suggest_int("n_independent", 1, 5),
+            "n_shared": trial.suggest_int("n_shared", 1, 5),
+            "lambda_sparse": trial.suggest_float("lambda_sparse", 1e-6, 1e-3, log=True),
+            "optimizer_params": dict(lr=trial.suggest_float("lr", 1e-3, 0.1, log=True)),
+        }
+
+    k_features = trial.suggest_int("k", 50, 200, step=25)
 
     f1_scores = []
 
-    for inner_fold, (train_idx, val_idx) in enumerate(cv.split(X, y, groups), start=1):
-        run_name = f"outer_{outer_fold}_inner_{inner_fold}_trial_{trial.number}"
-        group_name_wandb = f"{outer_fold}_trial_{trial.number}"
+    for fold, (train_idx, val_idx) in enumerate(cv.split(X, y, groups), start=1):
+        run_name = f"cv_fold_{fold}_trial_{trial.number}"
+        group_name_wandb = f"{fold}_trial_{trial.number}"
 
         X_tr, X_val = X.iloc[train_idx], X.iloc[val_idx]
         y_tr, y_val = y[train_idx], y[val_idx]
@@ -52,13 +68,7 @@ def objective(trial, X, y, groups, cv, outer_fold, wandb_dir):
             [
                 ("imputer", SimpleImputer(strategy="mean")),
                 ("scaler", MinMaxScaler()),
-                (
-                    "feature_selection",
-                    SelectKBest(
-                        score_func=mutual_info_classif,
-                        k=trial.suggest_int("k", 50, 200, step=25),
-                    ),
-                ),
+                ("feature_selection", SelectKBest(score_func=mutual_info_classif, k=k_features)),
             ]
         )
 
@@ -75,33 +85,56 @@ def objective(trial, X, y, groups, cv, outer_fold, wandb_dir):
             group=group_name_wandb,
             dir=wandb_dir,
         )
-        model = xgb.XGBClassifier(
-            **params,
-            n_estimators=1000,  # i.e., number of epochs
-            objective="binary:logistic",
-            eval_metric="logloss",
-            tree_method="hist",
-            n_jobs=32,
-            verbosity=0,
-            random_state=18,
-            early_stopping_rounds=50,
-            callbacks=[
-                XGBoostPruningCallback(trial, "validation_0-logloss"),
-                wandb.xgboost.WandbCallback(),
-            ],
-        )
 
-        model.fit(
-            X_tr_transformed,
-            y_tr,
-            eval_set=[(X_val_transformed, y_val)],
-        )
+        # ------- initialize and fit model -------
+        if model_type == "xgboost":
+            model = xgb.XGBClassifier(
+                **params,
+                n_estimators=1000,  # i.e., number of epochs
+                objective="binary:logistic",
+                eval_metric="logloss",
+                tree_method="hist",
+                n_jobs=32,
+                verbosity=0,
+                random_state=18,
+                early_stopping_rounds=50,
+                callbacks=[
+                    XGBoostPruningCallback(trial, "validation_0-logloss"),
+                    wandb.xgboost.WandbCallback(),
+                ],
+            )
 
+            model.fit(
+                X_tr_transformed,
+                y_tr,
+                eval_set=[(X_val_transformed, y_val)],
+            )
+            
+        elif model_type == "tabnet":
+            model = TabNetClassifier(
+                **params,
+                seed=18,
+                verbose=0,
+                device_name='cuda' if torch.cuda.is_available() else 'cpu'
+            )
+
+            model.fit(
+                X_train=X_tr_transformed, y_train=y_tr,
+                eval_set=[(X_val_transformed, y_val)],
+                eval_name=['val'],
+                eval_metric=['logloss'],
+                max_epochs=200, 
+                patience=30,
+                batch_size=256, virtual_batch_size=128
+            )
+
+        # ------ evaluate model ------
         preds = model.predict(X_val_transformed)
         f1_scores.append(f1_score(y_val, preds, zero_division=0))
 
         wandb.log({"f1_score": f1_scores[-1]})
         wandb.finish()
+
         trial.report(f1_score(y_val, preds, zero_division=0), len(f1_scores))
         if trial.should_prune():
             raise optuna.TrialPruned()
